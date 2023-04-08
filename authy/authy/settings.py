f33ac0dd9136f9c496a58bc019b4865ad30017a2
Akolade <emmakolade@gmail.com>
2023-04-08 23:43:07 +0100

    settings

--- authy/authy/settings.py
+++ authy/authy/settings.py
@@ -141,9 +141,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-mail.outlook.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = str(os.getenv('EMAIL_HOST_USER'))
-EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_HOST_PASSWORD'))
-
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('PASS')
 
 SWAGGER_SETTINGS = {
     'SECURITY_DEFINITIONS': {
